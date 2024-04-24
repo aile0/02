6aee9188590077147367d490fc67ab738c0ea031
勒 艾 <i_le2022@outlook.com>
2024-04-24 18:18:37 +0800

    0-100

--- 02.cpp
+++ 02.cpp
@@ -4,7 +4,6 @@
 #include<iostream>
 #include<mpir.h>
 #include<mpirxx.h>
-#include<boost/winapi/thread_pool.hpp>
 
 int main()
 {
@@ -12,30 +11,38 @@ int main()
 	std::cout << "hello,world!" << std::endl;
 }
 
-//int main()
-//{
-//
-//
-//    std::fstream td("D:\\Desktop\\vs_file\\e1\\testdata.txt");
-//    if (td.is_open())
-//    {
-//        std::cout << "打开成功" << std::endl;
-//    }
-//    double t = 0;
-//    std::random_device seed;
-//    std::default_random_engine e(seed());
-//    std::uniform_real_distribution<double> g(-10000, 10000);
-//    for (int i = 0; i < 1000; i++)
-//    {
-//        for (int j = 0; j < 5; j++)
-//        {
-//            t = g(e);
-//            td << t << " ";
-//        }
-//        t = g(e);
-//        td << t << std::endl;
-//    }
-//    td.close();
-//    std::cout << "Hello World!\n";
-//}
+int main()
+{
+    std::cout << "输入一个0-100的数字：" << std::endl;
+    int temp;
+    std::cin >> temp;
+    while (temp >= 0 && temp <= 100)
+    {
+        std::cout << "输入的数字符合要求" << std::endl;
+        std::cout << "请再次输入数字："<< std::endl;
+        std::cin >> temp;
+    }
+
+    //std::fstream td("D:\\Desktop\\vs_file\\e1\\testdata.txt");
+    //if (td.is_open())
+    //{
+    //    std::cout << "打开成功" << std::endl;
+    //}
+    //double t = 0;
+    //std::random_device seed;
+    //std::default_random_engine e(seed());
+    //std::uniform_real_distribution<double> g(-10000, 10000);
+    //for (int i = 0; i < 1000; i++)
+    //{
+    //    for (int j = 0; j < 5; j++)
+    //    {
+    //        t = g(e);
+    //        td << t << " ";
+    //    }
+    //    t = g(e);
+    //    td << t << std::endl;
+    //}
+    //td.close();
+    //std::cout << "Hello World!\n";
+}
 
