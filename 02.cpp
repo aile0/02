764e98438e4351d0c064ca62f8d0d4becd855f70
勒 艾 <i_le2022@outlook.com>
2024-04-24 18:21:39 +0800

    a

--- 02.cpp
+++ 02.cpp
@@ -1,9 +1,4 @@
-﻿//#include <iostream>
-//#include<fstream>
-//#include<random>
-#include<iostream>
-#include<mpir.h>
-#include<mpirxx.h>
+﻿#include<iostream>
 
 int main()
 {
@@ -16,27 +11,5 @@ int main()
         std::cout << "请再次输入数字："<< std::endl;
         std::cin >> temp;
     }
-
-    //std::fstream td("D:\\Desktop\\vs_file\\e1\\testdata.txt");
-    //if (td.is_open())
-    //{
-    //    std::cout << "打开成功" << std::endl;
-    //}
-    //double t = 0;
-    //std::random_device seed;
-    //std::default_random_engine e(seed());
-    //std::uniform_real_distribution<double> g(-10000, 10000);
-    //for (int i = 0; i < 1000; i++)
-    //{
-    //    for (int j = 0; j < 5; j++)
-    //    {
-    //        t = g(e);
-    //        td << t << " ";
-    //    }
-    //    t = g(e);
-    //    td << t << std::endl;
-    //}
-    //td.close();
-    //std::cout << "Hello World!\n";
 }
 
